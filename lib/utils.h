ed1ca3d7e21113884c64cc3eecf9f1612918e49a
felix-wang-0307 <hwang779@usc.edu>
2024-11-17 15:01:31 -0800

    feat: implement the authentication logic of ServerM and some utils

---
+++ lib/utils.h
@@ -0,0 +1,55 @@
+//
+// Created by Waterdog on 2024/11/17.
+//
+
+#ifndef GIT450_UTILS_H
+#define GIT450_UTILS_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <map>
+
+namespace utils {
+    std::map<std::string, std::string> loadConfig(const std::string& filename) {
+        std::ifstream config_file(filename);
+        std::map<std::string, std::string> config;
+        if (!config_file.is_open()) {
+            throw std::runtime_error("Failed to open configuration file.");
+        }
+
+        std::string line;
+        while (std::getline(config_file, line)) {
+            std::istringstream line_stream(line);
+            std::string key, value;
+            if (std::getline(line_stream, key, '=') && std::getline(line_stream, value)) {
+                config[key] = value;
+            }
+        }
+
+        return config;
+    }
+
+    std::vector<std::string> split(const std::string& str, char delimiter = ' ') {
+        std::vector<std::string> tokens;
+        std::string token;
+        std::istringstream tokenStream(str);
+        while (std::getline(tokenStream, token, delimiter)) {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+
+    std::string getOperation(const std::string& data) {
+        return split(data)[0];
+    }
+
+    std::string getPayload(const std::string& data) {
+        return data.substr(data.find(' ') + 1);
+    }
+}
+
+
+
+#endif //GIT450_UTILS_H
--- serverM.cpp
+++ serverM.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include "serverM.h"
+#include "lib/tcpSocket.h"
+#include "lib/udpSocket.h"
+#include "lib/utils.h"
 
-void bootUp() {
-    std::cout << "Server M is up and running using UDP on port " << UDP_PORT << std::endl;
+class ServerM {
+public:
+    TCPServerSocket* server;
+    UDPClientSocket* udpClient;
+    ServerM() {
+        bootUp();
+    }
+    ~ServerM() {
+        delete server;
+    }
+    void bootUp() {
+        server = new TCPServerSocket(TCP_PORT);
+        std::cout << "Server M is up and running using UDP on port " << UDP_PORT << std::endl;
+    }
+    void handleClientAuth() {
+
+    }
+    void run() {
+        server->listen();
+        while (true) {
+            TCPSocket* client = server->accept();
+            std::string data = client->receive();
+            if (data.empty()) {
+                break;
+            }
+            std::string operation = utils::getOperation(data);
+            if(operation == "AUTH") {
+                handleClientAuth();
+            }
+            client->send("Server M received the data");
+        }
+    }
 };
 
+
 int main() {
-    bootUp();
+    ServerM serverM;
+    serverM.run();
     return 0;
 }
