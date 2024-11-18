278e97fa2e26443d72a8866de74d5773cc3b7780
felix-wang-0307 <hwang779@usc.edu>
2024-11-17 21:49:32 -0800

    feat: initially implement the client

--- client.cpp
+++ client.cpp
@@ -1,11 +1,58 @@
 #include <iostream>
+#include "client.h"
+#include "lib/tcpSocket.h"
+#include "lib/utils.h"
 
-void bootUp() {
-    std::cout << "The client is up and running" << std::endl;
+enum ClientType {
+    GUEST,
+    USER
 };
 
-int main() {
-    bootUp();
+class Client {
+public:
+    TCPClientSocket* client;
+    int type;
+    Client(std::string username, std::string password) {
+        bootUp();
+        type = authenticate(username, password);
+        if (type == -1) {
+            std::cerr << "Failed to authenticate" << std::endl;
+            exit(1);
+        }
+    }
+    ~Client() {
+        delete client;
+    }
+    void bootUp() {
+        client = new TCPClientSocket();
+        std::cout << "Client is up and running" << std::endl;
+    }
+    int authenticate(const std::string& username, const std::string& password) {
+        std::string data = "AUTH " + username + " " + password;
+        client->send(data);
+        std::string response = client->receive();
+        if (response == "USER") {
+            return USER;
+        } else if (response == "GUEST") {
+            return GUEST;
+        } else {
+            return -1;
+        }
+    }
+    void run() {
+        while (true) {
+
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    // Run the client: ./client <username> <password>
+    if (argc < 3) {
+        std::cerr << "Usage: ./client <username> <password>" << std::endl;
+        exit(1);
+    }
+    Client client(argv[1], argv[2]);
     return 0;
 }
 
