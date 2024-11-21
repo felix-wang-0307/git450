ba347754df54554648f786bf80c82980ac8e8454
felix-wang-0307 <hwang779@usc.edu>
2024-11-20 16:55:54 -0800

    refactor: integrate configuration loading for server connection and improve authentication handling

--- client.cpp
+++ client.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <map>
 #include <thread>
 #include "client.h"
 #include "lib/tcp_socket.h"
 #include "lib/utils.h"
 
+std::map<std::string, std::string> config;
+
 enum ClientType {
+    INVALID = -1,
     GUEST,
     USER
 };
@@ -16,40 +20,39 @@ public:
     Client(std::string username, std::string password) {
         bootUp();
         type = authenticate(username, password);
-       if (type == -1) {
-           std::cerr << "Failed to authenticate" << std::endl;
-           exit(1);
-       }
+        if (type == -1) {
+            std::cerr << "Failed to authenticate" << std::endl;
+            exit(1);
+        }
     }
     ~Client() {
         delete client;
     }
     void bootUp() {
         client = new TCPClientSocket();
+        std::string server_ip = config["server_ip"];
+        int server_port = std::stoi(config["server_port"]);
         std::cout << "Client is up and running" << std::endl;
-        if (!client->connect("127.0.0.1", 25012)) {
+        if (!client->connect(server_ip, server_port)) {
             std::cerr << "Failed to connect to the server" << std::endl;
             exit(1);
         }
-        client->send("Hello, Server!");
-
-        std::string response = client->receive();
-        std::cout << "Received response: " << response << std::endl;
     }
     int authenticate(const std::string& username, const std::string& password) {
+        // Send the username and password to the server
         std::string data = "AUTH " + username + " " + password;
         client->send(data);
         std::string response = client->receive();
+        // Check the response: USER or GUEST
         if (response == "USER") {
             return USER;
         } else if (response == "GUEST") {
             return GUEST;
         } else {
-            return -1;
+            return INVALID;
         }
     }
     void run() {
-        // connect to the serverM
         while (true) {
 
         }
@@ -58,7 +61,9 @@ public:
 
 int main(int argc, char* argv[]) {
     // Run the client: ./client <username> <password>
+    config = utils::loadConfig();
     if (argc < 3) {
+        std::cerr << "Wrong number of arguments: expect 2, get " << argc << std::endl;
         std::cerr << "Usage: ./client <username> <password>" << std::endl;
         exit(1);
     }
