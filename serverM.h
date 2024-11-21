158a983e1a49dc9135c7f1d533de8fbba1d3cf56
felix-wang-0307 <hwang779@usc.edu>
2024-11-20 17:12:09 -0800

    refactor: remove unused header files for client and server components

--- serverM.h
+++
@@ -1,7 +0,0 @@
-#ifndef GIT450_SERVERM_H
-#define GIT450_SERVERM_H
-
-const int UDP_PORT = 24012;
-const int TCP_PORT = 25012;
-
-#endif //GIT450_SERVERM_H
--- serverR.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef GIT450_SERVERR_H
-#define GIT450_SERVERR_H
-
-const int PORT = 22012;
-
-#endif //GIT450_SERVERR_H
