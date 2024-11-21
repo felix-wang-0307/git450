d858a356ecb460f8f224a124ddd18aa0ac2be8aa
felix-wang-0307 <hwang779@usc.edu>
2024-11-20 22:32:27 -0800

    refactor: remove loadConfig function from utils, add toAstrix function for string masking

--- lib/utils.h
+++ lib/utils.h
@@ -9,25 +9,6 @@
 #include <map>
 
 namespace utils {
-    std::map<std::string, std::string> loadConfig(const std::string& filename = "config.txt") {
-        std::ifstream config_file(filename);
-        std::map<std::string, std::string> config;
-        if (!config_file.is_open()) {
-            throw std::runtime_error("Failed to open configuration file.");
-        }
-
-        std::string line;
-        while (std::getline(config_file, line)) {
-            std::istringstream line_stream(line);
-            std::string key, value;
-            if (std::getline(line_stream, key, '=') && std::getline(line_stream, value)) {
-                config[key] = value;
-            }
-        }
-
-        return config;
-    }
-
     std::vector<std::string> split(const std::string& str, char delimiter = ' ') {
         std::vector<std::string> tokens;
         std::string token;
@@ -45,8 +26,10 @@ namespace utils {
     std::string getPayload(const std::string& data) {
         return data.substr(data.find(' ') + 1);
     }
-}
 
-std::map<std::string, std::string> config = utils::loadConfig();
+    std::string toAstrix(const std::string &input) {
+        return std::string(input.size(), '*');
+    }
+}
 
 #endif //GIT450_UTILS_H
